12f00ca771d04c488d2ee3f9f28b2d5641f2a979
Manav Rathi <manav@mnvr.in>
2025-12-01 21:30:06 +0530

    clean

--- 01.py
+++ 01.py
@@ -1,20 +1,17 @@
 import sys
-import math
 
 pos = 50
 z1 = 0
 z2 = 0
-rotations = [(s[0], int(s[1:])) for s in sys.stdin]
-print(' ', 0, pos, z1, z2)
-for (d, steps) in rotations:
-    (m, p) = divmod(pos + ((-1 if d == 'L' else 1) * steps), 100)
+input = [(-1 if s[0] == 'L' else 1) * int(s[1:]) for s in sys.stdin]
+for steps in input:
+    (m, p) = divmod(pos + steps, 100)
     if m < 0 and pos == 0:
         m += 1
     if m > 0 and p == 0:
         m -= 1
-    z1 = z1 + (1 if p == 0 else 0)
-    z2 = z2 + (1 if p == 0 else 0) + abs(m)
+    z1 += 1 if p == 0 else 0
+    z2 += abs(m)
     pos = p
-    print(d, steps, pos, z1, z2, m)
 
-print(pos, z1, z2)
+print(z1, z1 + z2)
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-cat inputs/01-a | python 01.py
-echo 1097 7101
+set -o errexit
+
+diff <(cat inputs/01-a | python 01.py) <(echo "1097 7101")
