b71f7184d7689bfaa901a0bb38837932c87264da
Manav Rathi <manav@mnvr.in>
2025-12-02 13:43:40 +0530

    2

--- 02.py
+++ 02.py
@@ -1,6 +1,7 @@
 import sys
 
 iv = 0
+iv2 = 0
 for r in sys.stdin.read().strip().split(','):
     (u, v) = [int(s) for s in r.split('-')]
     for x in range(u, v + 1):
@@ -10,4 +11,19 @@ for r in sys.stdin.read().strip().split(','):
             continue
         if s[0:n//2] == s[n//2:]:
             iv += x
-print(iv)
+    for x in range(u, v + 1):
+        s = str(x)
+        n = len(s)
+        for i in range(n//2, 0, -1):
+            a = s[0:i]
+            j = i
+            while j < n:
+                b = s[j:i+j]
+                if a != b:
+                    break
+                j += i
+            if j == n:
+                iv2 += x
+                break
+
+print(iv, iv2)
--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 set -o errexit
 
 diff <(cat inputs/01 | python 01.py) <(echo "1097 7101")
-diff <(cat inputs/02 | python 02.py) <(echo "13108371860")
+diff <(cat inputs/02 | python 02.py) <(echo "13108371860 22471660255")
