c7cc7aa81565826005e6ecca5ed9128c7384d08a
Manav Rathi <manav@mnvr.in>
2025-12-02 21:01:43 +0530

    Mirror
    https://mastodon.social/@jukkasuomela.fi@bsky.brid.gy/115650037243506402

--- 01.py
+++ 01.py
@@ -1,14 +1,14 @@
 import sys
 
 pos, z1, z2 = 50, 0, 0
-for steps in [(-1 if s[0] == 'L' else 1) * int(s[1:]) for s in sys.stdin]:
-    (m, p) = divmod(pos + steps, 100)
-    if m < 0 and pos == 0:
-        m += 1
-    if m > 0 and p == 0:
-        m -= 1
-    z1 += 1 if p == 0 else 0
-    z2 += abs(m)
-    pos = p
+for s in sys.stdin:
+    if s[0] == 'L':
+        pos = (100 - pos) % 100
+    pos += int(s[1:])
+    z2 += pos // 100
+    pos %= 100
+    z1 += 1 if pos == 0 else 0
+    if s[0] == 'L':
+        pos = (100 - pos) % 100
 
-print(z1, z1 + z2)
+print(z1, z2)
--- 01.rs
+++ 01.rs
@@ -1,27 +1,22 @@
 fn main() {
     let (mut pos, mut z1, mut z2) = (50, 0, 0);
     for line in std::io::stdin().lines().map(|s| s.unwrap()) {
-        let (head, tail) = line.split_at(1);
-        let d = if head == "L" { -1 } else { 1 };
-        let steps = tail.parse::<i32>().unwrap() * d;
+        let (d, s) = line.split_at(1);
+        let rot = s.parse::<i32>().unwrap();
 
-        let npos = pos + steps;
-
-        let m = npos.div_euclid(100);
-        let p = npos.rem_euclid(100);
-
-        if m < 0 && pos == 0 {
-            z2 -= 1;
-        }
-        if m > 0 && p == 0 {
-            z2 -= 1;
+        if d == "L" {
+            pos = (100 - pos) % 100;
         }
 
-        z1 += if p == 0 { 1 } else { 0 };
-        z2 += m.abs();
+        pos += rot;
+        z2 += pos / 100;
+        pos %= 100;
+        z1 += if pos == 0 { 1 } else { 0 };
 
-        pos = p;
+        if d == "L" {
+            pos = (100 - pos) % 100;
+        }
     }
 
-    println!("{} {}", z1, z1 + z2);
+    println!("{} {}", z1, z2);
 }
