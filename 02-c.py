954ec25af01ecfb3e87d5518e61469f4d4b7f3f0
Manav Rathi <manav@mnvr.in>
2025-12-03 11:44:45 +0530

    odd

--- 02-c.py
+++ 02-c.py
@@ -1,28 +1,20 @@
 def rep(n):
+    if n % 2 == 1: return rep_odd(n)
     match n:
-        case 1: return rep1()
         case 2: return rep2()
-        case 3: return rep3()
         case 4: return rep4()
-        case 5: return rep5()
         case 6: return rep6()
 
-def rep1():
-    return []
+def rep_odd(n):
+    return [sum([c * 10**i for i in range(0, n)]) for c in range(1, 10)]
 
 def rep2():
     return [c*10 + c for c in range(1, 10)]
-
-def rep3():
-    return [c*100 + c*10 + c for c in range(1, 10)]
 
 def rep4():
     r2 = [c*10 + d for c in range(1, 10)
                    for d in range(0, 10) if c != d] + rep(2)
     return sorted([r*100 + r for r in r2])
-
-def rep5():
-    return [c*1000 + c*100 + c*10 + c for c in range(1, 10)]
 
 def rep6():
     r2 = [c*10 + d for c in range(1, 10)
@@ -41,10 +33,9 @@ def check_range(a, b):
             print(r)
 
 # print([v for n in range(1, 5) for v in rep(n)])
-# print(rep6())
 # print(check_range(11, 22))
-print(check_range(95, 115))
+# print(check_range(95, 115))
 print(check_range(998, 1012))
-# print(check_range(1188511880, 1188511890))
+# # print(check_range(1188511880, 1188511890))
 print(check_range(222220, 222224))
-print(check_range(446443, 446449))
+# print(check_range(446443, 446449))
