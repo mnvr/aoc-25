1617059fa48fcb856e23fe68f3089fbff00bf197
Manav Rathi <manav@mnvr.in>
2025-12-03 11:28:59 +0530

    wire in

--- 02-c.py
+++ 02-c.py
@@ -3,8 +3,9 @@ def rep(n):
         case 1: return rep1()
         case 2: return rep2()
         case 3: return rep3()
-        case 4: return rep1()
-        case 5: return rep1()
+        case 4: return rep4()
+        case 5: return rep5()
+        case 6: return rep6()
 
 def rep1():
     return []
@@ -25,11 +26,25 @@ def rep5():
 
 def rep6():
     r2 = [c*10 + d for c in range(1, 10)
-                   for d in range(0, 10) if c != d] + rep(2)
+                   for d in range(0, 10)]
     r3 = [c*100 + d*10 + e for c in range(1, 10)
                            for d in range(0, 10)
-                           for e in range(0, 10) if c != d] + rep(3)
-    return sorted([r*10000 + r*100 + r for r in r2])
+                           for e in range(0, 10)]
+    r6 = [r*10000 + r*100 + r for r in r2] + [r*1000 + r for r in r3]
+    return sorted(set(r6))
+
+def check_range(a, b):
+    rs = set(rep(len(str(a))) + rep(len(str(b))))
+    span = range(a, b + 1)
+    for r in rs:
+        if r in span:
+            print(r)
 
 # print([v for n in range(1, 5) for v in rep(n)])
-print(rep6())
+# print(rep6())
+# print(check_range(11, 22))
+print(check_range(95, 115))
+print(check_range(998, 1012))
+# print(check_range(1188511880, 1188511890))
+print(check_range(222220, 222224))
+print(check_range(446443, 446449))
