a96079c40ad669c9514658b11ec1be122ca74266
Manav Rathi <manav@mnvr.in>
2025-12-03 12:11:37 +0530

    even 1

--- 02-c.py
+++ 02-c.py
@@ -4,26 +4,39 @@ def rep(n):
         case 2: return rep2()
         case 4: return rep4()
         case 6: return rep6()
+        case 8: return rep8()
 
 def rep_odd(n):
     return [sum([c * 10**i for i in range(0, n)]) for c in range(1, 10)]
 
+def _r1():
+    return [c*10**1 + c for c in range(1, 10)]
+
+def _r2():
+    return [c*10**1 + d for c in range(1, 10)
+                        for d in range(0, 10)]
+
+def _r3():
+    return [c*10**2 + d*10**1 + e for c in range(1, 10)
+                                  for d in range(0, 10)
+                                  for e in range(0, 10)]
+
+def _r4():
+    return [c*10**3 + d*10**2 + e*10**1 + f for c in range(1, 10)
+                                            for d in range(0, 10)
+                                            for e in range(0, 10)
+                                            for f in range(0, 10)]
 def rep2():
-    return [c*10 + c for c in range(1, 10)]
+    return _r1()
 
 def rep4():
-    r2 = [c*10 + d for c in range(1, 10)
-                   for d in range(0, 10) if c != d] + rep(2)
-    return sorted([r*100 + r for r in r2])
+    return [r*10**2 + r for r in _r2()]
 
 def rep6():
-    r2 = [c*10 + d for c in range(1, 10)
-                   for d in range(0, 10)]
-    r3 = [c*100 + d*10 + e for c in range(1, 10)
-                           for d in range(0, 10)
-                           for e in range(0, 10)]
-    r6 = [r*10000 + r*100 + r for r in r2] + [r*1000 + r for r in r3]
-    return sorted(set(r6))
+    return [r*10**4 + r*10**2 + r for r in _r2()] + [r*10**3 + r for r in _r3()]
+
+def rep8():
+    return [r*10**4 + r for r in _r4()] + [r*10**4 + r for r in rep4()]
 
 def check_range(a, b):
     rs = set(rep(len(str(a))) + rep(len(str(b))))
@@ -32,10 +45,11 @@ def check_range(a, b):
         if r in span:
             print(r)
 
-# print([v for n in range(1, 5) for v in rep(n)])
-# print(check_range(11, 22))
-# print(check_range(95, 115))
-print(check_range(998, 1012))
-# # print(check_range(1188511880, 1188511890))
-print(check_range(222220, 222224))
-# print(check_range(446443, 446449))
+# print(rep(8))
+check_range(11, 22)
+check_range(95, 115)
+check_range(998, 1012)
+# check_range(1188511880, 1188511890)
+check_range(222220, 222224)
+check_range(446443, 446449)
+check_range(38593856, 38593862)
