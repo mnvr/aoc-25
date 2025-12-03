1e583a8e1a87ff148c632fccf2927b1531c1d20d
Manav Rathi <manav@mnvr.in>
2025-12-03 21:16:25 +0530

    10

--- 02-c.py
+++ 02-c.py
@@ -14,17 +14,16 @@ def rep2():
     return [c*10**1 + c for c in range(1, 10)]
 
 def rep4():
-    return [r*10**2 + r for r in range(1, 10**2)]
+    return [r*10**2 + r for r in range(10, 100)]
 
 def rep6():
-    return [r*10**4 + r*10**2 + r for r in range(1, 10**2)] + [r*10**3 + r for r in range(1, 10**3)]
+    return [r*10**4 + r*10**2 + r for r in range(10, 100)] + [r*10**3 + r for r in range(100, 1000)]
 
 def rep8():
-    return [r*10**4 + r for r in range(1, 10**4)] + [r*10**4 + r for r in rep4()]
+    return [r*10**4 + r for r in range(1000, 10000)] #+ [r*10**4 + r for r in rep4()]
 
-# def rep10():
-#     return [r*10**8 + r*10**6 + r*10**4 + r**10*2 + r for r in _r2()] + \
-#            [r*10**5 + r for r in rep(5)]
+def rep10():
+    return [r*10**8 + r*10**6 + r*10**4 + r*10**2 + r for r in range(10, 100)] + [r*10**5 + r for r in range(10000, 100000)]
 
 def check_range(a, b):
     rs = set(rep(len(str(a))) + rep(len(str(b))))
@@ -39,4 +38,5 @@ check_range(998, 1012)
 check_range(222220, 222224)
 check_range(446443, 446449)
 check_range(38593856, 38593862)
-# check_range(1188511880, 1188511890)
+check_range(1188511880, 1188511890)
+# print(sorted(rep(10)))
