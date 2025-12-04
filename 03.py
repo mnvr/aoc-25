817c13d085d72d32864992173e9c5a343574415d
Manav Rathi <manav@mnvr.in>
2025-12-04 20:41:33 +0530

    clean

--- 03.py
+++ 03.py
@@ -1,6 +1,6 @@
 import sys
 
-def make_num(m, ns):
+def num(m, ns):
     for n in ns:
         m *= 10
         m += n
@@ -9,40 +9,22 @@ def make_num(m, ns):
 p1, p2 = 0, 0
 for line in sys.stdin:
     xs = [int(c) for c in line.rstrip()]
+
     m = max(xs[:-1])
-    n = None
-    ns = []
-    min_ns = 10
-    for x in xs:
-        if n is not None:
-            n = max(n, x)
-        if x == m:
-            if n is None:
-                n = 0
+    n = 0
+    for x in xs[xs.index(m)+1:]:
+        n = max(n, x)
     p1 += m*10 + n
 
     m = max(xs[:-11])
-    n = None
-    ns = []
-    min_ns = 10
-    for x in xs:
-        if n is not None:
-            # print(m, len(ns), x, min_ns, ns)
-            if len(ns) < 11:
-                ns.append(x)
-                min_ns = min(min_ns, x)
-            else:
-                ns0 = list(ns)
-                for i in range(0, len(ns0)):
-                    ns2 = list(ns)
-                    del ns2[i]
-                    ns2.append(x)
-                    if make_num(m, ns0) < make_num(m, ns2):
-                        ns0 = ns2
-                ns = ns0
-        if x == m:
-            if n is None:
-                n = 0
-    p2 += make_num(m, ns)
+    mi = xs.index(m)
+    ns = xs[mi+1:mi+12]
+    for x in xs[mi+12:]:
+        for i in range(0, 11):
+            ns2 = ns[:i] + ns[i+1:] + [x]
+            if num(m, ns) < num(m, ns2):
+                ns = ns2
+                break
+    p2 += num(m, ns)
 
 print(p1, p2)
