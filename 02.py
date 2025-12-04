f4dda6050b47d75040b7052c4de2af4a67c5b3a5
Manav Rathi <manav@mnvr.in>
2025-12-04 10:26:11 +0530

    Cleanup

--- 02.py
+++ 02.py
@@ -1,23 +1,55 @@
+def rep(n):
+    match n:
+        case 1: return []
+        case 2: return rep2()
+        case 3: return rep1(n)
+        case 4: return rep4()
+        case 5: return rep1(n)
+        case 6: return rep6()
+        case 7: return rep1(n)
+        case 8: return rep8()
+        case 9: return rep9()
+        case 10: return rep10()
+
+def rep_even(n):
+    return [] if n % 2 == 1 else rep(n)
+
+def rep1(n):
+    return [sum([c * 10**i for i in range(0, n)]) for c in range(1, 10)]
+
+def rep2():
+    return [c*10**1 + c for c in range(1, 10)]
+
+def rep4():
+    return [r*10**2 + r for r in range(10, 100)]
+
+def rep6():
+    return [r*10**4 + r*10**2 + r for r in range(10, 100)] \
+           + [r*10**3 + r for r in range(100, 1000)]
+
+def rep8():
+    return [r*10**4 + r for r in range(1000, 10000)]
+
+def rep9():
+    return [r*10**6 + r*10**3 + r for r in range(100, 1000)]
+
+def rep10():
+    return [r*10**8 + r*10**6 + r*10**4 + r*10**2 + r for r in range(10, 100)] \
+           + [r*10**5 + r for r in range(10000, 100000)]
+
+def check_range(s):
+    (a, b) = [int(x) for x in s.split('-')]
+    rs = set(rep(len(str(a))) + rep(len(str(b))))
+    all = [r for r in rs if r in range(a, b + 1)]
+    even = [r for r in all if r//(h := 10**(len(str(r))//2)) == r%h]
+    return (even, all)
+
 import sys
 
-iv, iv2 = 0, 0
+p1, p2 = 0, 0
 for r in sys.stdin.read().strip().split(','):
-    (u, v) = [int(s) for s in r.split('-')]
-    for x in range(u, v + 1):
-        s = str(x)
-        n = len(s)
-        for i in range(n//2, 0, -1):
-            a = s[0:i]
-            j = i
-            while j < n:
-                b = s[j:i+j]
-                if a != b:
-                    break
-                j += i
-            if j == n:
-                if i + i == n:
-                    iv += x
-                iv2 += x
-                break
+    (even, all) = check_range(r)
+    p1 += sum(even)
+    p2 += sum(all)
 
-print(iv, iv2)
+print(p1, p2)
--- README.md
+++ README.md
@@ -25,6 +25,13 @@ hyperfine --input inputs/01 'python 01.py'
 hyperfine --input inputs/01 ./01
 ```
 
-| Day  | Python    | Rust      |
-| ---- | --------- | --------- |
-| `01` | `  19 ms` | `   4 ms` |
+| Day | Python | Rust |
+| --- | -----: | ---: |
+| 1   |  19 ms | 4 ms |
+| 2   | 110 ms |    - |
+
+## Ahas
+
+- **[Day 1](https://mastodon.social/@jukkasuomela.fi@bsky.brid.gy/115650037243506402)** - "Always rotate right, flip the dial if needed"
+
+- **[Day 2](https://www.reddit.com/r/adventofcode/comments/1pc0le8/2025_day_2_part_2_time_to_reach_for_that_trusty/nrup8b2/)** - "You only have to construct the values that are made out of repeated digit strings and then see if the result is the range, not check every number in the range to see if it's made of repeated digit strings"
