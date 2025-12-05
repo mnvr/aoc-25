c47ef7f8526d973134043df72af3922379fc5235
Manav Rathi <manav@mnvr.in>
2025-12-05 22:38:35 +0530

    p1

--- 04.py
+++ 04.py
@@ -4,4 +4,18 @@ mx = []
 for line in sys.stdin:
     mx.append([0 if c == '.' else 1 for c in line.strip()])
 
-print(mx)
+t = 0
+for y in range(0, len(mx)):
+    for x in range(0, len(mx[0])):
+        if mx[y][x] != 1:
+            continue
+        s = 0
+        for j in range(max(0, y-1), min(y+2, len(mx))):
+            for i in range(max(0, x-1), min(x+2, len(mx[0]))):
+                if not (y == j and x == i):
+                    s += (mx[j][i])
+        if s < 4:
+            t += 1
+
+print(t)
+exit()
--- test.sh
+++ test.sh
@@ -5,5 +5,6 @@ set -o errexit
 diff <(cat inputs/01 | python 01.py) <(echo "1097 7101")
 diff <(cat inputs/02 | python 02.py) <(echo "13108371860 22471660255")
 diff <(cat inputs/03 | python 03.py) <(echo "17031 168575096286051")
+diff <(cat inputs/04 | python 04.py) <(echo "1604")
 
 diff <(rustc 01.rs && cat inputs/01 | ./01) <(echo "1097 7101")
