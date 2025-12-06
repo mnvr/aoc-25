6c84914fed7a22d945f499005b1050e886948f0a
Manav Rathi <manav@mnvr.in>
2025-12-06 13:48:51 +0530

    clean

--- 04.py
+++ 04.py
@@ -17,20 +17,6 @@ def neighbour_count(mx, y, x):
             c += mx[j][i]
     return c
 
-def pretty(mx):
-    return '\n'.join([''.join(map(str, row)) for row in mx])
-
-def pretty2(nx, ns):
-    BOLD = '\033[1m'
-    END = '\033[0m'
-    for (y, row) in enumerate(nx):
-        for (x, n) in enumerate(row):
-            if (y, x) in ns:
-                print(BOLD + str(n) + END, end='')
-            else:
-                print(n, end='')
-        print()
-
 def relax(mx, nx):
     changed_mx = []
     changed_nx = []
@@ -45,13 +31,11 @@ def relax(mx, nx):
         mx[y][x] -= 1
     for (y, x) in changed_nx:
         nx[y][x] -= 1
-    return (mx, nx, len(changed_mx))
+    return len(changed_mx)
 
 nx = neighbours(mx)
 counts = []
-while True:
-    (mx, nx, c) = relax(mx, nx)
-    if not c: break
+while c := relax(mx, nx):
     counts.append(c)
 
 print(counts[0], sum(counts))
