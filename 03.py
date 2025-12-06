efd90bbe8ec9ab5a8edf7b1857fce6c2259bf5c7
Manav Rathi <manav@mnvr.in>
2025-12-06 09:29:15 +0530

    Extend

--- 03.py
+++ 03.py
@@ -12,17 +12,23 @@ s1, s2 = 0, 0
 for line in sys.stdin:
     xs = [int(c) for c in line.rstrip()]
 
-    pos = 0
-    remaining = 2
-    batteries = []
-    while remaining > 0:
-        best = max(xs[pos:len(xs)-remaining+1])
-        i = xs.index(best, pos, len(xs)-remaining+1)
-        pos = i
-        batteries.append(best)
-        remaining -= 1
-    z = ''.join(map(str, batteries))
-    print(z)
+    def find(remaining):
+        pos = 0
+        batteries = []
+        while remaining > 0:
+            best = max(xs[pos:len(xs)-remaining+1])
+            i = xs.index(best, pos, len(xs)-remaining+1)
+            pos = i + 1
+            batteries.append(best)
+            remaining -= 1
+        z = ''.join(map(str, batteries))
+        z = int(z)
+        print(z)
+        return z
+
+    print(xs)
+    s1 += find(2)
+    s2 += find(12)
 
     m = max(xs[:-1])
     p1 += m*10 + max(xs[xs.index(m)+1:])
@@ -40,3 +46,4 @@ for line in sys.stdin:
     p2 += num(m, ns)
 
 print(p1, p2)
+print(s1, s2)
