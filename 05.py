83278382f7b3fdec327a5c3d07efd5d0a7d9289b
Manav Rathi <manav@mnvr.in>
2025-12-06 07:59:11 +0530

    vis

--- 05.py
+++ 05.py
@@ -16,4 +16,39 @@ for line in sys.stdin:
                 fc += 1
                 break
 
-print(fc)
+def merge(ri, rj):
+    u, v = ri.start, ri.start + ri.stop
+    x, y = rj.start, rj.start + rj.stop
+    if v < x or y < u:
+        return None
+    else:
+        u = min(u, x)
+        v = max(v, y)
+        return range(u, v)
+
+def key(r):
+    return (r.start, r.stop)
+
+merged = {}
+for ri in ranges:
+    for rj in ranges:
+        ki, kj = key(ri), key(rj)
+        print(f"Considering {ki}, {kj}:", end=' ')
+        rm = merge(ri, rj)
+        if rm is None:
+            merged[key(ri)] = ri
+            print(f"no overlap, adding {ki}")
+        else:
+            km = key(rm)
+            print(f"overlap, adding {km}")
+            if key(ri) in merged:
+                del merged[key(ri)]
+                print(f"Removed {ki}")
+            if key(rj) in merged:
+                del merged[key(rj)]
+                print(f"Removed {kj}")
+            merged[key(rm)] = rm
+
+# print(fc)
+from pprint import pprint
+pprint(merged)
