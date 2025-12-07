55e1787948de3733ad95b9f66bb06f4b622551d4
Manav Rathi <manav@mnvr.in>
2025-12-07 12:22:01 +0530

    2 pass

--- 06.py
+++ 06.py
@@ -1,36 +1,27 @@
 import sys
 from math import prod
 
+nlen = None
+lines = []
+for line in sys.stdin:
+    lines.append(line)
+    nl = [len(c) for c in line.split()]
+    nlen = map(max, zip(nl, nlen if nlen else nl))
+nlen = list(nlen)
+
 def to_num(xs):
     n = 0
     for x in xs:
         n = n * 10 + x
     return n
 
-nums = None
 result = 0
-for line in sys.stdin:
-    print(line)
-    if nums is None:
-        nums = []
-        count = 0
-        i = 0
-        while i < len(line):
-            while i < len(line) and line[i] == ' ':
-                i += 1
-                count += 1
-            if i < len(line):
-                while i < len(line) and line[i] != ' ':
-                    i += 1
-                    count += 1
-                print("num len", count)
-                nums.append(list([[] for _ in range(0, count)]))
-                count = 0
-                i += 1
-
+nums = [list([[] for _ in range(0, l)]) for l in nlen]
+# print(nums)
+for line in lines:
     if line[0] in ['+', '*']:
         for (i, s) in enumerate(line.split()):
-            print(nums[i])
+            # print(nums[i])
             match s:
                 case '+': result += sum(map(to_num, nums[i]))
                 case '*': result += prod(map(to_num, nums[i]))
@@ -40,11 +31,11 @@ for line in sys.stdin:
             if c != ' ' and c != '\n':
                 nums[a][b].append(int(c))
             b += 1
-            if b > len(nums[a]):
+            if b > nlen[a]:
                 b = 0
                 a += 1
 
-    print(nums)
+    # print(nums)
 
 print(result)
 exit()
