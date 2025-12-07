92e66ef91a2f50238c04946922fa1261ad53ec7d
Manav Rathi <manav@mnvr.in>
2025-12-07 11:38:47 +0530

    p2 example

--- 06.py
+++ 06.py
@@ -1,77 +1,50 @@
 import sys
 from math import prod
-import re
+
+def to_num(xs):
+    n = 0
+    for x in xs:
+        n = n * 10 + x
+    return n
 
 nums = None
-# nums = [[0]*3 for _ in range(0, 4)]
-# nums = [list([] for _ in range(0, 3)) for _ in range(0, 4) ]
-# print(nums)
 result = 0
 for line in sys.stdin:
     print(line)
     if nums is None:
         nums = []
         count = 0
-        # if line[-1] == '\n':
-            # line = line[:-1]
-        for (i, c) in enumerate(line):
-            print(i, c, count, end=': ')
-            if c == ' ' and i > 0 and line[i-1] != ' ':
-                print('append to nums')
-                nums.append(list([[] for _ in range(0, count)]))
-                count = 0
-            else:
-                print('incr count')
+        i = 0
+        while i < len(line):
+            while i < len(line) and line[i] == ' ':
+                i += 1
                 count += 1
-        nums.append(list([[] for _ in range(0, count)]))
-
-    print(nums)
-    exit()
-
-    if line[-1] == '\n':
-        line = line[:-1]
-    if nums is None:
-        nums = []
-        count = 0
-        for (i, c) in enumerate(line):
-            if c == ' ' and i+1 < len(line) and line[i+1] != ' ':
+            if i < len(line):
+                while i < len(line) and line[i] != ' ':
+                    i += 1
+                    count += 1
+                print("num len", count)
                 nums.append(list([[] for _ in range(0, count)]))
                 count = 0
-    elif line[0] in ['+', '*']:
+                i += 1
+
+    if line[0] in ['+', '*']:
         for (i, s) in enumerate(line.split()):
+            print(nums[i])
             match s:
-                case '+': result += sum(nums[i])
-                case '*': result += prod(nums[i])
+                case '+': result += sum(map(to_num, nums[i]))
+                case '*': result += prod(map(to_num, nums[i]))
     else:
-        print(nums)
         a, b = 0, 0
-        for (i, c) in enumerate(line):
-            if c != ' ':
+        for c in line:
+            if c != ' ' and c != '\n':
                 nums[a][b].append(int(c))
-                b += 1
-            else:
-                if i+1 < len(line) and line[i+1] != ' ':
-                    nums[a][b].append(int(c))
-                    b += 1
-                else:
-                    b = 0
-                    a += 1
+            b += 1
+            if b > len(nums[a]):
+                b = 0
+                a += 1
 
     print(nums)
-    continue
-
-    for (i, s) in enumerate(line.split()):
-        print(nums)
-        match s:
-            case '+': result += sum(nums[i])
-            case '*': result += prod(nums[i])
-            case _:
-                for (j, c) in enumerate(reversed(s)):
-                    # print(i, j, c, nums)
-                    nums[i][j].append(int(c))
-                    # nums[i][j] = nums[i][j]*10 + int(c)
-    # print(nums)
-    # exit()
 
 print(result)
 exit()
