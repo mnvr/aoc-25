fb1f6dcd5ba61ba2fc17f201d44037fe12ef7fba
Manav Rathi <manav@mnvr.in>
2025-12-08 14:17:56 +0530

    p1 optim

--- 08.py
+++ 08.py
@@ -5,21 +5,27 @@ from heapq import heappush, heappop
 boxes = [tuple(map(int, line.split(','))) for line in sys.stdin]
 
 dist = []
+circuits = {}
 for (i, x) in enumerate(boxes):
+    circuits[x] = set([x])
     for (j, y) in enumerate(boxes):
         if j > i:
             d = sqrt(sum(map(lambda uv: (uv[0] - uv[1])**2, zip(x, y))))
             heappush(dist, (d, (x, y)))
 
-circuits = []
 for _ in range(0, 1000):
-    _, pair = heappop(dist)
-    new_circuit = set([pair[0], pair[1]])
-    for c in circuits:
-        if pair[0] in c or pair[1] in c:
-            new_circuit = new_circuit.union(c)
-    circuits = [c for c in circuits if not (pair[0] in c or pair[1] in c)]
-    circuits.append(new_circuit)
+    _, (p, q) = heappop(dist)
+    s1 = circuits[p]
+    s2 = circuits[q]
+    s3 = s1.union(s2)
+    for n in s3:
+        circuits[n] = s3
+
+new_circuits = []
+for c in circuits.values():
+    if c not in new_circuits:
+        new_circuits.append(c)
+circuits = new_circuits
 
 p1 = prod(sorted(map(len, circuits), reverse=True)[:3])
 print(p1)
