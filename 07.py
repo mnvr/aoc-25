58d3b55566bc0308c0ddd58d0191b3c5f0dc7df6
Manav Rathi <manav@mnvr.in>
2025-12-08 11:04:31 +0530

    p2

--- 07.py
+++ 07.py
@@ -1,6 +1,8 @@
 import sys
+from collections import Counter
 
 xs = set()
+paths = Counter()
 xm = 0
 p1 = 0
 for line in sys.stdin:
@@ -9,9 +11,13 @@ for line in sys.stdin:
         match c:
             case 'S':
                 xs.add(i)
+                paths[i] = 1
             case '^':
                 if i in xs:
                     p1 += 1
+                    paths[i-1] += paths[i]
+                    paths[i+1] += paths[i]
+                    del paths[i]
                     # print(f"removing {i} adding {i - 1} and {i + 1}")
                     xs.remove(i)
                     xs.add(i - 1)
@@ -21,3 +27,4 @@ for line in sys.stdin:
 xs = filter(lambda x: x >= 0 and x <= xm, xs)
 # print(len(list(xs)))
 print(p1)
+print(paths.total())
