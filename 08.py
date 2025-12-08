c8680b6e453b4e56cd0676c6fb43e8080d262527
Manav Rathi <manav@mnvr.in>
2025-12-08 13:56:55 +0530

    heap
    28 s => 0.6 s

--- 08.py
+++ 08.py
@@ -1,5 +1,6 @@
 import sys
 from math import prod, sqrt
+from heapq import heappush, heappop
 
 boxes = [tuple(map(int, line.split(','))) for line in sys.stdin]
 
@@ -10,16 +11,15 @@ for (i, x) in enumerate(boxes):
             d = sqrt(sum(map(lambda uv: (uv[0] - uv[1])**2, zip(x, y))))
             dist[(x, y)] = d
 
+heap = []
+
+for k, v in dist.items():
+    heappush(heap, (v, k))
+
 circuits = []
 for _ in range(0, 1000):
-    min_dist = int(1e9)
-    min_dist_pair = None
-    for key, value in dist.items():
-        if value < min_dist:
-            min_dist = value
-            min_dist_pair = key
-    del dist[min_dist_pair]
-    pair = min_dist_pair
+    item = heappop(heap)
+    pair = item[1]
     new_circuit = set([pair[0], pair[1]])
     for c in circuits:
         if pair[0] in c or pair[1] in c:
