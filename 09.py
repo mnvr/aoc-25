ffcb7cf18618712aab950059baff2389f8de9ccb
Manav Rathi <manav@mnvr.in>
2025-12-10 18:43:25 +0530

    floodfill

--- 09.py
+++ 09.py
@@ -7,9 +7,12 @@ tiles = [list(map(int, line.split(','))) for line in sys.stdin]
 def dist(a, b):
     return (abs(a[0]-b[0])+1) * (abs(a[1]-b[1])+1)
 
-ds = (dist(a, b) for (a, b) in combinations(tiles, 2))
+# ds = (dist(a, b) for (a, b) in combinations(tiles, 2))
+ds = ((dist(a, b), (a, b)) for (a, b) in combinations(tiles, 2))
+ds = list(sorted(ds, reverse=True))
 
-p1 = max(ds)
+# p1 = max(ds)
+p1 = ds[0][0]
 print(p1)
 
 vertical_lines = defaultdict(list)
@@ -28,9 +31,9 @@ for start, end in zip(tiles, tiles[1:] + [tiles[0]]):
 # horizontal_lines = dict(horizontal_lines)
 # vertical_lines = dict(vertical_lines)
 
-print(max_x, max_y)
-print(horizontal_lines)
-print(vertical_lines)
+# print(max_x, max_y)
+# print(horizontal_lines)
+# print(vertical_lines)
 
 def print1(s):
     # print(s)
@@ -44,6 +47,61 @@ def is_point_on_boundary(x, y):
     for a, b in vertical_lines[x]:
         if a <= y and y <= b:
             return True
+
+def vis(s):
+    print("012345678901234")
+    for y in range(0, max_y + 3):
+        if y < 10:
+            print(f"{y}", end='')
+        else:
+            print(" ", end='')
+        for x in range(0, max_x + 3):
+            if (x, y) in s:
+                print('*', end='')
+            elif is_point_on_boundary(x, y):
+                print('#', end='')
+            else:
+                print('.', end='')
+            # else:
+            #     print('\033[37m#\033[0m', end='')
+            # print('#' if is_point_inside(x, y) else '.', end='')
+        print()
+    print()
+
+def is_wall(x, y):
+    return any(a <= y and y <= b for a, b in vertical_lines[x]) or \
+        any(a <= x and x <= b for a, b in horizontal_lines[y])
+
+def neighbours(x, y):
+    ds = [-1, 0, +1]
+    return [(x+u, y+v) for u in ds for v in ds if (u != 0 or v != 0) and (x+u) >= 0 and (x+u) <= max_x and (y+v) >= 0 and (y+v) <= max_y]
+
+def floodfill_out():
+    v = set()
+    q = [(0, 0)]
+    while len(q):
+        (x, y) = q.pop()
+        # vis(v)
+        for n in neighbours(x, y):
+            if n not in v and not is_wall(n[0], n[1]):
+                v.add(n)
+                q.append(n)
+    return v
+
+outside = floodfill_out()
+# print(outside)
+# vis(outside)
+
+for (dist, (a, b)) in ds:
+    (x, y) = a
+    (u, v) = b
+    if (x, v) in outside or (u, y) in outside:
+        # print(f"skipping {(a, b)}")
+        continue
+    print(dist)
+    break
+
+exit()
 
 def is_point_inside(x, y):
     res = _is_point_inside(x, y)
