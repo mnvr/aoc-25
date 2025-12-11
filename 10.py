b6b9924728f143f2af431ebcd0f42de3d07248ad
Manav Rathi <manav@mnvr.in>
2025-12-11 14:12:07 +0530

    shortest_path p1 7s => 200ms

--- 10.py
+++ 10.py
@@ -8,7 +8,7 @@ def parse(line):
     joltage = tuple(int(c) for c in joltage[1:-1].split(','))
     return (lights, buttons, joltage)
 
-def steps(dest, buttons, action):
+def make_neighbours(buttons, action):
     def neighbours(v):
         ns = set()
         for button in buttons:
@@ -17,7 +17,10 @@ def steps(dest, buttons, action):
                 n[i] = action(n[i])
             ns.add(tuple(n))
         return ns
+    return neighbours
 
+def steps(dest, buttons, action):
+    neighbours = make_neighbours(buttons, action)
     start = tuple([0] * len(dest))
     visited = set()
     queue = collections.deque([(start, 0)])
@@ -31,8 +34,41 @@ def steps(dest, buttons, action):
             if v not in visited:
                 queue.append((v, d+1))
 
+def shortest_path(dest, buttons, action):
+    neighbours = make_neighbours(buttons, action)
+    start = tuple([0] * len(dest))
+    inf = 10000
+
+    queue = set()
+    visited = set()
+    dist = collections.defaultdict(lambda: inf)
+    queue.add(start)
+    dist[start] = 0
+
+    # vertex in Q with minimum dist[u]
+    def min_q():
+        d, u = inf, None
+        for v in queue:
+            if d > dist[v]:
+                (d, u) = (dist[v], v)
+        queue.remove(u)
+        return u
+
+    while len(queue):
+        u = min_q()
+        visited.add(u)
+        if u == dest:
+            return dist[u]
+        for v in neighbours(u):
+            if v not in visited:
+                queue.add(v)
+            alt = dist[u] + 1
+            if dist[v] > alt:
+                dist[v] = alt
+
 def process(lights, buttons, joltage):
-    s1 = steps(lights, buttons, lambda v: (v + 1) % 2)
+    # s1 = steps(lights, buttons, lambda v: (v + 1) % 2)
+    s1 = shortest_path(lights, buttons, lambda v: (v + 1) % 2)
     # s2 = steps(joltage, buttons, lambda v: v + 1)
     return s1
 
